6dd75867c8c7af3bde07d396cb6960465b147d6c
Gideon22iboyi <your-email@example.com>
2025-04-30 14:55:47 +0100

    feat: configure CORS middleware with specific options for enhanced security

--- express.js
+++ express.js
@@ -16,7 +16,11 @@ const cors = require('cors');
 const app= express();
 
 
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
